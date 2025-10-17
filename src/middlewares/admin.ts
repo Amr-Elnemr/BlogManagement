90e3f28699687ebd9e3d6e4ad8d63157e8fda08f
Amr Essam <amr.essam@vodafone.com>
2025-10-18 01:51:39 +0300

    chore: remove testing and sample file

--- src/middlewares/admin.ts
+++
@@ -1,9 +0,0 @@
-import { type Request, type Response, type NextFunction } from "express";
-
-export default (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user?.isAdmin) {
-    return res.status(403).send("Access denied");
-  }
-
-  next();
-};
--- src/router/home.ts
+++
@@ -1,9 +0,0 @@
-import express, { type Request, type Response } from "express";
-const router = express.Router();
-import path from "path";
-
-router.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
-export default router;
